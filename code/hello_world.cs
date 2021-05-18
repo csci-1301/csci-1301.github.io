2788e34d836b38a8906f05832bc2b9c7bdf61ee7
Clément Aubert <caubert@augusta.edu>
2021-05-18 00:40:00 -0400

    Added comments to first source code, lecture 2.

--- code/hello_world.cs
+++ code/hello_world.cs
@@ -1,7 +1,10 @@
+/* I'm a multi-line comment,
+ * I can span over multiple lines!
+ */
 using System;
 
 class Program {
     static void Main() {
-        Console.WriteLine("Hello, world!");
+        Console.WriteLine("Hello, world!"); // I'm an in-line comment.
     }
 }
