16d3bb9c9d94010622ab0bfbcfbdffa6e2209da2
Clément Aubert <caubert@augusta.edu>
2021-05-31 13:03:31 -0400

    Added rectangle source code.

---
+++ labs/Rectangle/src/Rectangle/Rectangle/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * CSCI 1301
+ * A simple application program for the Rectangle class
+ */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle myRectangle; // Declaration of a Rectangle variable.
+        myRectangle = new Rectangle(); // Assignment, we are creating a Rectangle object and assigning it to our Rectangle variable.
+
+        // Now we set the instance variables for this object, using the accessors
+        myRectangle.SetLength(12); // Accessor to set the Length
+        myRectangle.SetWidth(3); // Accessor to set the Width
+
+        /*
+         * Let us use the three other methods to 
+         *  a. Retrieve the Length (using GetLength())
+         *  b. Retrieve the Width (using GetWidth())
+         *  c. Compute the Area (using ComputeArea())
+         */
+
+        Console.WriteLine($"You rectangle's length is {myRectangle.GetLength()}" + // a.
+            $", its width is {myRectangle.GetWidth()}" + //b.
+            $", so its area is {myRectangle.ComputeArea()}."); //c.
+    }
+}
---
+++ labs/Rectangle/src/Rectangle/Rectangle/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * CSCI 1301
+ * A class: the Rectangle class
+ */
+
+class Rectangle
+{
+    /*
+     * Declaration of the two instance variables / attributes / fields
+     */
+    private int length;
+    private int width;
+
+    /*
+     * Accessors for the Length
+     */
+
+    // "Setter" (or "mutator") for the Length:
+    public void SetLength(int lengthParameter)
+    {
+        length = lengthParameter;
+    }
+    // "Getter" (or "accessor") for the Length:
+    public int GetLength()
+    {
+        return length;
+    }
+
+    /*
+     * Accessors for the Width
+     */
+
+     // "Setter" (or "mutator") for the Width:
+    public void SetWidth(int widthParameter)
+    {
+        width = widthParameter;
+    }
+    // "Getter" (or "accessor") for the Width:
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    /*
+     * Method to compute the area of a rectangle
+     */
+    public int ComputeArea()
+    {
+        return length * width;
+
+    }
+}
