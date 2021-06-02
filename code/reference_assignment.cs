a52f55fd68f59d1f5b58c4889574727785b8ab1b
C.J. Tran <84515899+CJ-Tran@users.noreply.github.com>
2021-06-02 11:42:59 -0400

    Update reference_assignment.cs
    Added class keyword and class name, then adjusted indentation for existing code

--- code/reference_assignment.cs
+++ code/reference_assignment.cs
@@ -1,13 +1,16 @@
 using System;
-static void Main(string[] args)
+class Program
 {
-    Rectangle rect1 = new Rectangle();
-    rect1.SetLength(8);
-    rect1.SetWidth(10);
-    Rectangle rect2 = rect1;
-    rect2.SetLength(4);
-    Console.WriteLine($"Rectangle 1: {rect1.GetLength()} "
-        + $"by {rect1.GetWidth()}");
-    Console.WriteLine($"Rectangle 2: {rect2.GetLength()} "
-        + $"by {rect2.GetWidth()}");
+    static void Main(string[] args)
+    {
+        Rectangle rect1 = new Rectangle();
+        rect1.SetLength(8);
+        rect1.SetWidth(10);
+        Rectangle rect2 = rect1;
+        rect2.SetLength(4);
+        Console.WriteLine($"Rectangle 1: {rect1.GetLength()} "
+           + $"by {rect1.GetWidth()}");
+        Console.WriteLine($"Rectangle 2: {rect2.GetLength()} "
+           + $"by {rect2.GetWidth()}");
+    }
 }
