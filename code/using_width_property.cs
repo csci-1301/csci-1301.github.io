cdc5747e14e39e3480c07e55fd0cba795e51c82e
C.J. Tran <84515899+CJ-Tran@users.noreply.github.com>
2021-06-03 11:13:16 -0400

    Update using_width_property.cs

--- code/using_width_property.cs
+++ code/using_width_property.cs
@@ -7,7 +7,7 @@ class Program
         Rectangle myRectangle = new Rectangle();
         myRectangle.SetLength(6);
         myRectangle.Width = 15;
-        Console.WriteLine("Your rectangle's length is" +
+        Console.WriteLine("Your rectangle's length is " +
             $"{myRectangle.GetLength()}, and " +
             $"its width is {myRectangle.Width}");
     }
