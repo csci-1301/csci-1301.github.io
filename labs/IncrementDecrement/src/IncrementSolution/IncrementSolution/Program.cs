c41225785b4cb538a8a0b318e0e2c2353eecb7cb
Clément Aubert <caubert@augusta.edu>
2023-03-15 19:47:32 -0400

    Update Program.cs

--- labs/IncrementDecrement/src/IncrementSolution/IncrementSolution/Program.cs
+++ labs/IncrementDecrement/src/IncrementSolution/IncrementSolution/Program.cs
@@ -11,36 +11,36 @@ public class Program
         x = 5;
         n = x++;
         Console.WriteLine("n is now " + n + ".");
+        Console.WriteLine("x is now " + x + ".");
 
         // 2nd question
         // Resetting the value of x.
         x = 5;
         n = ++x;
         Console.WriteLine("n is now " + n + ".");
+        Console.WriteLine("x is now " + x + ".");
 
         // 3rd question
         // Resetting the value of x.
         x = 5;
-        n = x++ + x++;
+        n = x + x++;
+        // So, the order is:
+        // 1. Sum x and x,
+        // 2. Assign the result (10) to n,
+        // 3. Increment x.
         Console.WriteLine("n is now " + n + ".");
+        Console.WriteLine("x is now " + x + ".");
 
         // 4th question
         // Resetting the value of x.
         x = 5;
         n = ++x + ++x;
+        // So, we the order is:
+        // 1. Increment x (it is now 6),
+        // 2. Increment x again (it is now 7)
+        // 3. Sum the two values of x *at the time they were read*
+        // (that is, sum 6 and 7)
         Console.WriteLine("n is now " + n + ".");
-
-        // 5th question
-        // Resetting the value of x.
-        x = 5;
-        int y = 6;
-        n = x++ * ++y;
-        Console.WriteLine("n is now " + n + ".");
-
-        // 6th question
-        // Resetting the value of x.
-        x = 5;
-        n = x++ + --x;
-        Console.WriteLine("n is now " + n + ".");
+        Console.WriteLine("x is now " + x + ".");
     }
 }
