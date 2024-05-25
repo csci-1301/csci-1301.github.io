bfce5892414035709b714267a798ba11b5a190fe
Clément <six@localhost.localdomain>
2024-05-25 18:28:51 -0400

    Custom message for 404 error page.

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -6,6 +6,10 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <p>Please, <a href="https://github.com/csci-1301/csci-1301.github.io/issues/new?body=The+following+page+is+in+error+(_please,+insert+here+the+address+of+the+page+you+were+trying+to+access_).&title=Link+in+error+on+website">report the issue on github</a>.</p>
+      {
+        // Link generated using https://github.com/sindresorhus/new-github-issue-url
+      }
     </article>
   )
 }
